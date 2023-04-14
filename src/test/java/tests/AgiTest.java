8ae8ce12118cde4f010d2c6f5bb35c6330e8e642
Amanda Mendes <amandamendes@Amandas-MacBook-Air.local>
2023-04-13 22:43:46 -0300

    subindo automacao de teste web

---
+++ src/test/java/tests/AgiTest.java
@@ -0,0 +1,32 @@
+package tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.sql.SQLOutput;
+
+public class AgiTest  {
+    @Test
+    public void TestValidarCampoPesquisa(){
+        System.setProperty("webdriver.chrome.driver", "/Users/amandamendes/drivers/chromedriver");
+        WebDriver navegador = new ChromeDriver();
+
+        navegador.get("https://blogdoagi.com.br");
+
+        navegador.findElement(By.id("search-open")).click();
+        navegador.findElement(By.className("search-field")).click();
+        navegador.findElement(By.className("search-field")).sendKeys("Teste da Amanda");
+        navegador.findElement(By.className("search-submit")).click();
+
+        String telaPesquisa = navegador.findElement(By.className("entry-title")).getText();
+
+        Assert.assertEquals("Nenhum resultado", telaPesquisa);
+
+        if (telaPesquisa != null){
+            System.out.println("Teste efetuado com sucesso!");
+        }
+    }
+}
